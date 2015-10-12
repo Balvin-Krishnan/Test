0c5b6d5461a4eaa30707ec03b9769748042444a0
Balvin <kjbalvin@gmail.com>
2015-10-12 12:54:37 -0400

    test
    test

---
+++ src/com/balvin/multithreading/JunitTesting.java
@@ -0,0 +1,12 @@
+package com.balvin.multithreading;
+/* 
+ * This class is for Junit testing the application
+ */
+public class JunitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
